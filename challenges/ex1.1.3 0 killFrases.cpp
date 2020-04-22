8a743e88dbaaa7cdb5da3c211d28a547d4676128
rfcosta85 <rfcosta85@gmail.com>
2020-04-21 21:14:06 -0300

    Create ex1.1.3 0 killFrases.cpp

---
+++ challenges/ex1.1.3 0 killFrases.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	
+	// #300ExercisesChallenge - 02/300
+	// The instruction will show killsPhrases
+	
+	char killsPhrases[256] = "Forget this man!\nThis is not going to work\nYou'll never make it\nEverything is wrong ";	
+	
+	cout << killsPhrases;
+	
+	return 0;
+}
